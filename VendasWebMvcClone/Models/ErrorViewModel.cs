df76092710db96ab7ade17d0b6212063a7bb5c82
WennerLeonn <wennersalvatore@gmail.com>
2019-09-22 15:13:54 -0400

    Comecando o projeto

---
+++ VendasWebMvcClone/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VendasWebMvcClone.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
